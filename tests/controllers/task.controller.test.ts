429673ce7fcfacfc05c91f88ecbe5aa5ece5b4f7
Rikhu Surya <surya.rikhu@gmail.com>
2025-07-26 22:11:42 +0200

    Add unit tests for controller

---
+++ tests/controllers/task.controller.test.ts
@@ -0,0 +1,34 @@
+import { createTaskHandler } from '../../src/controllers/task.controller';
+import * as taskService from '../../src/services/task.service';
+import { Request, Response } from 'express';
+
+describe('Task Controller', () => {
+    const mockRequest = {
+        body: {
+            title: 'Controller Test',
+            description: 'Test Desc',
+            status: 'pending',
+        },
+    } as Request;
+
+    const mockResponse = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+    } as any as Response;
+
+    it('should call service and respond with 201', async () => {
+        const fakeTask = {
+            _id: 'xyz123',
+            ...mockRequest.body,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        };
+
+        jest.spyOn(taskService, 'createTask').mockResolvedValue(fakeTask);
+
+        await createTaskHandler(mockRequest, mockResponse);
+
+        expect(mockResponse.status).toHaveBeenCalledWith(201);
+        expect(mockResponse.json).toHaveBeenCalledWith(fakeTask);
+    });
+});
