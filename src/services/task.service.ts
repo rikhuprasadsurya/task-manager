339edf39deb6a2b28b4350b739c47112deead0ee
Rikhu Surya <surya.rikhu@gmail.com>
2025-07-28 15:39:27 +0200

    Add redis to get cached tasks and delete the cache when a new task is inserted

--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -1,12 +1,28 @@
+import redis from '../utils/redis';
 import {ITask, Task} from '../models/task.model';
 
 export type CreateTaskInput = Pick<ITask, 'title' | 'description' | 'status'>;
+export const cacheKey = 'tasks:all';
 
 export const getTasks = async (): Promise<ITask[]> => {
     return [];
 };
 
+export const getCachedTasks = async () => {
+    const cached = await redis.get(cacheKey);
+    if (cached) return JSON.parse(cached);
+
+    const tasks = await Task.find().lean();
+    await redis.set(cacheKey, JSON.stringify(tasks), 'EX', 60); // 1 min TTL
+
+    return tasks;
+};
+
 export const createTask = async (data: Partial<CreateTaskInput>): Promise<ITask> => {
     const task = new Task(data);
-    return await task.save();
+    const savedTask = await task.save();
+
+    await redis.del(cacheKey);
+
+    return savedTask;
 };
--- tests/services/task.service.test.ts
+++ tests/services/task.service.test.ts
@@ -1,8 +1,18 @@
-import {createTask, CreateTaskInput} from '../../src/services/task.service';
+import {cacheKey, createTask, CreateTaskInput} from '../../src/services/task.service';
 import { Task } from '../../src/models/task.model';
+import redis from '../../src/utils/redis';
 
-// mock Task model
+
 jest.mock('../../src/models/task.model');
+jest.mock('ioredis', () => {
+    const mRedis = {
+        get: jest.fn(),
+        set: jest.fn(),
+        del: jest.fn(),
+        on: jest.fn(),
+    };
+    return jest.fn(() => mRedis);
+});
 
 describe('Task Service', () => {
     const mockTaskData: CreateTaskInput = {
@@ -24,10 +34,12 @@ describe('Task Service', () => {
         });
 
         (Task as any).mockImplementation(() => ({ save: mockSave }));
+        const redisDelSpy = jest.spyOn(redis, 'del');
 
         const result = await createTask(mockTaskData);
 
         expect(mockSave).toHaveBeenCalled();
+        expect(redisDelSpy).toHaveBeenCalledWith(cacheKey)
         expect(result.title).toBe('Mock Task');
     });
 });
