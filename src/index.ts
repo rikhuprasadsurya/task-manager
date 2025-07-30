83d3d15240df77e683faeefee7a857d42d430db8
Rikhu Surya <surya.rikhu@gmail.com>
2025-07-30 17:41:08 +0200

    Add health endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
-import express from 'express';
-import taskRoutes from "./routes/task.routes";
+import express, {Request, Response} from 'express';
+import taskRouter from "./routes/task.routes";
 
 const app = express();
 app.use(express.json());
 
-app.use('/api/tasks', taskRoutes);
+app.use('/api/tasks', taskRouter);
+
+//heath endpoint
+app.get('/health', (_req: Request, _res: Response) => {_res.send('API is running...')});
 
 export default app;
 
 if (require.main === module) {
     app.listen(3000, () => console.log('Server running on port 3000'));
 }
+
