f83ff3f8569b6a25b386a61df3e79cb9e6e97b8a
Rikhu Surya <surya.rikhu@gmail.com>
2025-08-02 16:05:11 +0200

    Add test for get all tasks

--- tests/controllers/task.controller.test.ts
+++ tests/controllers/task.controller.test.ts
@@ -1,39 +1,38 @@
-import { createTaskHandler } from '../../src/controllers/task.controller';
+import {createTaskHandler, getTaskHandler} from '../../src/controllers/task.controller';
 import * as taskService from '../../src/services/task.service';
 import { Request, Response } from 'express';
 import redis from '../../src/utils/redis';
-import {BadRequestError} from "../../src/utils/errors";
 
 afterAll(async () => {
     await redis.quit();
 });
 
 describe('Task Controller', () => {
-    const mockRequest = {
-        body: {
-            title: 'Controller Test',
-            description: 'Test Desc',
-            status: 'pending',
-        },
-    } as Request;
-
     const mockResponse = {
         status: jest.fn().mockReturnThis(),
         json: jest.fn(),
     } as any as Response;
 
     describe('POST /api/tasks', () => {
+        const mockRequestWithTask = {
+            body: {
+                title: 'Controller Test',
+                description: 'Test Desc',
+                status: 'pending',
+            },
+        } as Request;
+
         it('should create a task when a valid task is given', async () => {
             const mockServiceReturnValue = {
                 _id: 'xyz123',
-                ...mockRequest.body,
+                ...mockRequestWithTask.body,
                 createdAt: new Date(),
                 updatedAt: new Date(),
             };
 
             jest.spyOn(taskService, 'createTask').mockResolvedValue(mockServiceReturnValue);
 
-            await createTaskHandler(mockRequest, mockResponse);
+            await createTaskHandler(mockRequestWithTask, mockResponse);
 
             expect(mockResponse.status).toHaveBeenCalledWith(201);
             expect(mockResponse.json).toHaveBeenCalledWith(mockServiceReturnValue);
@@ -44,12 +43,54 @@ describe('Task Controller', () => {
 
             jest.spyOn(taskService, 'createTask').mockRejectedValue(error);
 
-            await createTaskHandler(mockRequest, mockResponse);
+            await createTaskHandler(mockRequestWithTask, mockResponse);
 
             expect(mockResponse.status).toHaveBeenCalledWith(400);
             expect(mockResponse.json).toHaveBeenCalledWith(
                 {error: 'invalid data'}
             );
+        });
+    });
+
+    describe('GET /api/tasks', () => {
+        const mockRequest = {
+            body: {},
+        } as Request;
+
+        it('should get all tasks', async () => {
+            const mockServiceReturnValue = [{
+                _id: 'xyz1',
+                title: 'Controller Test 1',
+                description: 'Test Desc 1',
+                status: 'pending',
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            }, {
+                _id: 'xyz2',
+                title: 'Controller Test 2',
+                description: 'Test Desc 2',
+                status: 'in-progress',
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            }];
+
+            jest.spyOn(taskService, 'getTasks').mockResolvedValue(mockServiceReturnValue);
+
+            await getTaskHandler(mockRequest, mockResponse);
+
+            expect(mockResponse.status).toHaveBeenCalledWith(200);
+            expect(mockResponse.json).toHaveBeenCalledWith(mockServiceReturnValue);
+        });
+
+        it('should return Bad Request 400 error when fetching tasks fails', async () => {
+            const error = new Error('some error');
+
+            jest.spyOn(taskService, 'getTasks').mockRejectedValue(error);
+
+            await getTaskHandler(mockRequest, mockResponse);
+
+            expect(mockResponse.status).toHaveBeenCalledWith(400);
+            expect(mockResponse.json).toHaveBeenCalledWith({error: error.message});
         });
     });
 });
